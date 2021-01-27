c89b22232102da77e5c82bf72ea40d3fc92b2245
akshit6830 <75782991+akshit6830@users.noreply.github.com>
2021-01-27 19:28:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(800,400);
+  a = createSprite(400, 200, 50, 50);
+  b = createSprite(400, 200, 30, 30);
+  a.shapeColor = "blue" ; 
+  b.shapeColor = "blue" ; 
+}
+
+function draw() {
+  background(255,255,255);  
+
+  a.x = World.mouseX;
+  a.y = World.mouseY;
+
+  if (a.x-b.x  <=  a.width/2+b.width/2 && 
+      a.y-b.y <= a.height/2+b.height/2 && 
+      b.x-a.x <= b.width/2+b.width/2 && 
+      b.y-a.y <= b.height/2+a.height/2) 
+  {
+    a.shapeColor = "red";
+    b.shapeColor = "red";
+  }
+  else {
+    a.shapeColor = "blue";
+    b.shapeColor = "blue";
+  }
+
+  drawSprites();
+}
